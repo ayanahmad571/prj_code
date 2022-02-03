dcd9d5233e832e1595e61e2add4725c5d727641a
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2022-02-03 18:47:40 +0000

    Fix DB Code and ID

--- data_prep/data_fetch.py
+++ data_prep/data_fetch.py
@@ -1,9 +1,22 @@
 import json
 
+p = "../../datasets/"
 
+paths = [
+    "botometer-feedback-2019/botometer-feedback-2019_tweets.json",
+    "pronbots-2019/pronbots-2019.json", 
+    "verified-2019/verified-2019.json", 
+    "vendor-purchased-2019/vendor-purchased-2019.json", 
+    "political-bots-2019/political-bots-2019.json", 
+    "celebrity-2019(1)/celebrity-2019.json", 
+    "botwiki-2019/botwiki-2019.json", 
+    "cresci-rtbust-2019(1)/cresci-rtbust-2019.json", 
+    "gilani-2017/gilani-2017.json", 
+    "midterm-2018(1)/midterm-2018.json"
+]
 
 # Opening JSON file
-f = open('test_data.json')
+f = open(p+paths[0])
  
 # returns JSON object as
 # a dictionary
@@ -11,7 +24,7 @@ data = json.load(f)
  
 # Iterating through the json
 # list
-# print(data)
+print(data)
  
 # Closing file
 f.close()
--- data_prep/db_connect.py
+++ data_prep/db_connect.py
@@ -11,7 +11,6 @@ mydb = mysql.connector.connect(
 # Create Table
 # CREATE TABLE `prj`.`data_lab` ( `id` INT(25) NOT NULL AUTO_INCREMENT , `uid` INT(100) NOT NULL , `bot` INT(100) NULL , `num_likes_per_num_friends` INT(100) NULL , `follow_friends_ratio` INT(100) NULL , `max_time_bw_retweets` INT(100) NULL , `re_per_tweet` INT(100) NULL , `num_likes` INT(100) NULL , `num_likes_per_follower` INT(100) NULL , `age_account` INT(100) NULL , `num_tweets` INT(100) NULL , `location` INT(100) NULL , `len_username` INT(100) NULL , `has_photo` INT(100) NULL , `likes_per_day` INT(100) NULL , PRIMARY KEY (`id`(25))) ENGINE = MyISAM;
 
-#ALTER TABLE `data_lab` ADD `id` INT(25) NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`);
 
 myscursor = mydb.cursor()
 # mycursor.execute("SELECT * FROM ms_views limit 100")
